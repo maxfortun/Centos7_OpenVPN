d08a6353f291daf08f230ca9e17f4bacb8c90cae
max <max@safetgate.localdomain>
2016-10-05 12:27:14 -0400

    Configs for tcp/udp

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Centos7_OpenVPN
+# OpenVPN
+
+Some scripts that are required for openvpn to work
+
+## SELinux
+If you have SELinux running, you may want to run openvpn_unpriv_hack.sh to allow log writing, script execution and sudo.
+
+
---
+++ learn-address.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+action=$1
+address=$2
+type=$3
+
+if [ -z "$address" ]; then
+	exit
+fi
+
+LOG=/var/run/openvpn/learn-address.log
+
+echo $(date '+%Y-%m-%d %H:%M:%S') $* >> $LOG
+env | xargs -L 1 echo $(date '+%Y-%m-%d %H:%M:%S') $address >> $LOG
+ 
+case $action in
+        add|update)
+		echo "$(date '+%Y-%m-%d %H:%M:%S') $address ip route del $address/32" >> $LOG
+                /usr/local/sbin/unpriv-ip route del $address/32 >> $LOG 2>&1 
+		echo "ip route add $address/32 dev $dev" >> $LOG
+                /usr/local/sbin/unpriv-ip route add $address/32 dev $dev >> $LOG 2>&1
+        ;;
+        delete)
+		echo "$(date '+%Y-%m-%d %H:%M:%S') $address ip route del $address/32" >> $LOG
+                /usr/local/sbin/unpriv-ip route del $address/32 >> $LOG 2>&1 
+        ;;
+        *)
+        ;;
+esac
+ 
+exit 0;
