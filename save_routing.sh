eb9f6dd92eade3764195a70fe8fcd90e67cbc61d
Max Fortun <max@maxf.net>
2017-01-27 23:16:26 -0500

    Added table name for each route saved

--- save_routing.sh
+++ save_routing.sh
@@ -4,7 +4,10 @@ iptables-save > /etc/sysconfig/iptables
 ip link show|grep -o -P '(?<=\d:\s)[^:]+'|grep -v '^lo$' | while read link; do
 	routes=$(ip route show table $link 2>/dev/null)
 	if [ -n "$routes" ]; then
-		echo "$routes" > /etc/sysconfig/network-scripts/route-$link
+		echo -n > /etc/sysconfig/network-scripts/route-$link
+		while read line; do
+			echo "$line table $link" >> /etc/sysconfig/network-scripts/route-$link
+		done <<< "$routes"
 	fi
 
 	rules=$(ip rule list 2>/dev/null | grep $link | sed 's/^[0-9]*:\s*//g')
