6f0c4fdcb5e0bd10c90072a1f0cf0ce39cc47108
Max Fortun <max@maxf.net>
2017-03-13 10:57:29 -0400

    Filling out templates

--- genOVPN.sh
+++ genOVPN.sh
@@ -13,9 +13,9 @@ PRI=${PRI//$'\n'/\\$'\n'}
 PRI=${PRI//$'\r'/\\$'\r'}
 
 for ovpn in *.ovpn; do
-	sed "s#your-publicly-accessible-ip-here#$wanIP#g" $ovpn
-	echo sed "s#... your ca cert here ...#$CA#g" $ovpn
-	sed "s#... your client public cert here ...#$PUB#g" $ovpn
-	sed "s#... your client private key here ...#$PRI#g" $ovpn
+	sed -i "s#your-publicly-accessible-ip-here#$wanIP#g" $ovpn
+	sed -i "s#... your ca cert here ...#$CA#g" $ovpn
+	sed -i "s#... your client public cert here ...#$PUB#g" $ovpn
+	sed -i "s#... your client private key here ...#$PRI#g" $ovpn
 done
 
