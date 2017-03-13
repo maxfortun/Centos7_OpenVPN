784cc496a205bd1075d830a1cb11cbdc125c2449
Max Fortun <max@maxf.net>
2017-03-13 10:50:26 -0400

    Replaced slashes with pounds to avoid conflict with file content that may posess slashes

--- genOVPN.sh
+++ genOVPN.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 wanIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
-CA=$(cat ca.crt)
-PUB=$(cat server.crt)
-PRI=$(cat server.key)
+CA=$(cat ca.crt | tr "\r\n" "\\\\r\\\\n")
+PUB=$(cat server.crt | tr "\r\n" "\\\\r\\\\n")
+PRI=$(cat server.key | tr "\r\n" "\\\\r\\\\n")
 
 for ovpn in *.ovpn; do
-	sed "s/your-publicly-accessible-ip-here/$wanIP/g" $ovpn
-	sed "s/... your ca cert here .../$CA/g" $ovpn
-	sed "s/... your client public cert here .../$PUB/g" $ovpn
-	sed "s/... your client private key here .../$PRI/g" $ovpn
+	sed "s#your-publicly-accessible-ip-here#$wanIP#g" $ovpn
+	echo sed "s#... your ca cert here ...#$CA#g" $ovpn
+	sed "s#... your client public cert here ...#$PUB#g" $ovpn
+	sed "s#... your client private key here ...#$PRI#g" $ovpn
 done
 
