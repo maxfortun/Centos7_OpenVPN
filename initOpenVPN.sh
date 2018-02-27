eb4bcdb620e79e51dd91ae524c3a15d7f0e69e17
Max <max@maxf.net>
2018-02-27 01:22:01 -0500

    check sestatus before running

--- initOpenVPN.sh
+++ initOpenVPN.sh
@@ -11,7 +11,8 @@ cd $SWD
 SWD=$(pwd)
 cd $CWD
 
-yum -y install epel-release openvpn easy-rsa policycoreutils-python iptables-services traceroute net-tools NetworkManager-config-routing-rules bind bind-utils iptables initscripts
+yum -y install epel-release 
+yum -y install openvpn easy-rsa policycoreutils-python iptables-services traceroute net-tools NetworkManager-config-routing-rules bind bind-utils iptables initscripts
 
 systemctl mask firewalld
 systemctl enable iptables
